56f2bcb2a56ba35c3da9f45b82bbdca33fb71d9e
ranaintizar <intizaralirana2@gmail.com>
2023-07-02 21:44:19 +0500

    Add Transition in Feature Text

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import clsx from "clsx";
 
 import Img from "assets/url-shortener.png";
+import ArrowIcon from "assets/arrow-right.svg";
 
 import stl from "./HeroSection.module.scss";
 
@@ -11,29 +12,47 @@ interface Props {
 }
 
 const HeroSection = ({ theme }: Props) => {
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+  const texts = [
+    "Simplifying Your Links",
+    "Amplifying Your Reach",
+    "Optimizing Your Sharing",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className={clsx(stl.heroSec, stl[`${theme}HeroSec`])}>
       <div className={stl.container}>
         <div className={stl.content}>
-          <h1 className={stl.heading}>Payments tool for software companies</h1>
+          <h1 className={stl.heading}>
+            Explore Our Advanced URL Shortener App,
+            <div className={stl.featureTransition}>
+              {texts.map((text, index) => (
+                <span
+                  key={index}
+                  className={clsx(
+                    stl.text,
+                    `${index === currentIndex ? stl.current : ""}`
+                  )}
+                >
+                  {text}
+                </span>
+              ))}
+            </div>
+          </h1>
           <p className={stl.desc}>
             From checkout to global sales tax compliance, companies around the
             world use Flowbite to simplify their payment stack.
           </p>
           <button className={stl.primaryBtn}>
-            Get started
-            <svg
-              className={stl.icon}
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
+            Get started <ArrowIcon />
           </button>
           <button className={stl.secondaryBtn}>Speak to Sales</button>
         </div>

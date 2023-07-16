6db552f2a556c7776c1bd62a8c27b0b1f9c22788
ranaintizar <intizaralirana2@gmail.com>
2023-07-16 20:33:41 +0500

    .

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 // import Overview from "./overview";
 import URLShortener from "components/url-shortener";
@@ -7,29 +6,6 @@ import URLShortener from "components/url-shortener";
 export default function Home() {
   //@ts-ignore
   const [theme, setTheme] = React.useState("light");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.post("/api/shorten", {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-
-        if (response.status === 200) {
-          const data = response.data;
-          console.log(data);
-        } else {
-          console.log("Error:", response.statusText);
-        }
-      } catch (error: any) {
-        console.log("Error:", error.message);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <main>

e9e3bc855a3060f793e47bb6506eee01d66249b0
ranaintizar <intizaralirana2@gmail.com>
2023-08-11 00:54:52 +0500

    .

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,24 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-// import LinkTable from "components/link-table";
 import WelcomeBanner from "components/welcome-banner";
 
 export default function Home() {
-  // useEffect(() => {
-  //   const searchInput = document.getElementById("searchInput");
-
-  //   document.addEventListener("keydown", (event) => {
-  //     if ((event.ctrlKey || event.metaKey) && event.key === "k") {
-  //       event.preventDefault();
-
-  //       searchInput?.focus();
-  //     }
-  //   });
-  // });
-
   return (
     <main>
-      <WelcomeBanner />
+      <WelcomeBanner theme="light" />
     </main>
   );
 }

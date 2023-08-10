001de2122b3b1caa3c9122d1cb236d1f7cfdab72
ranaintizar <intizaralirana2@gmail.com>
2023-08-11 00:54:52 +0500

    .

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,20 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import WelcomeBanner from "components/welcome-banner";
 
 export default function Home() {
-  // useEffect(() => {
-  //   const searchInput = document.getElementById("searchInput");
-
-  //   document.addEventListener("keydown", (event) => {
-  //     if ((event.ctrlKey || event.metaKey) && event.key === "k") {
-  //       event.preventDefault();
-
-  //       searchInput?.focus();
-  //     }
-  //   });
-  // });
-
   return (
     <main>
       <WelcomeBanner theme="light" />

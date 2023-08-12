0b405fe15a16999c0c7a27c679eb6a276b0f1c91
ranaintizar <intizaralirana2@gmail.com>
2023-08-13 04:26:57 +0500

    .

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import LinkTable from "components/link-table";
 
 export default function Home() {
+  useEffect(() => {
+    const searchInput = document.getElementById("searchInput");
+
+    document.addEventListener("keydown", (event) => {
+      if ((event.ctrlKey || event.metaKey) && event.key === "k") {
+        event.preventDefault();
+
+        searchInput?.focus();
+      }
+    });
+  });
   return (
     <main>
       <LinkTable theme="light" />

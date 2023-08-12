4c1aba2d8e073040530279556da1c76d088d95ef
ranaintizar <intizaralirana2@gmail.com>
2023-08-13 04:30:03 +0500

    .

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AuthForm from "components/auth-form";
 
 export default function Home() {
+  const [formType, setFormType] = useState("sign up");
   return (
     <main>
-      <AuthForm theme="light" />
+      <AuthForm theme="light" formType={formType} setFormType={setFormType} />
     </main>
   );
 }

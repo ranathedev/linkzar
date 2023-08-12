51244f7f484d711945dac135f4b7ddc0e9257c14
ranaintizar <intizaralirana2@gmail.com>
2023-08-13 03:28:24 +0500

    .

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import WelcomeBanner from "components/welcome-banner";
 
 export default function Home() {
+  useEffect(() => {
+    const searchInput = document.getElementById("searchInput");
+
+    document.addEventListener("keydown", (event) => {
+      if ((event.ctrlKey || event.metaKey) && event.key === "k") {
+        event.preventDefault();
+
+        searchInput?.focus();
+      }
+    });
+  });
   return (
     <main>
       <WelcomeBanner theme="light" />

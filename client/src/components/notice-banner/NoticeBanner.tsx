59e4db0bd25e9bda0c9e4eb2c542c338ec8aa72c
ranaintizar <intizaralirana2@gmail.com>
2024-01-23 16:51:08 +0500

    minor fixes

--- client/src/components/notice-banner/NoticeBanner.tsx
+++ client/src/components/notice-banner/NoticeBanner.tsx
@@ -6,9 +6,9 @@ const NoticeBanner = () => {
   return (
     <div className={stl.banner}>
       <p>
-        We're sorry, but our Application is currently experiencing technical
-        difficulties. Our team is working hard to resolve the issue and get
-        things back to normal as soon as possible. We apologize for any
+        We&apos;re sorry, but our Application is currently experiencing
+        technical difficulties. Our team is working hard to resolve the issue
+        and get things back to normal as soon as possible. We apologize for any
         inconvenience this may cause.
       </p>
       <p>Please check back later. Thank you for your patience.</p>

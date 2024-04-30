f11084da833f5ba190cd8bd154b32460bf8b8afd
ranaintizar <intizaralirana2@gmail.com>
2024-04-30 06:02:04 +0500

    minor fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import Layout from 'components/layout'
 import Homepage from 'components/homepage'
 
 export default function Home({ user, theme }: PageProps) {
+  console.log('Deployed from Github...')
+
   return (
     <Layout theme={theme} user={user} title="URL Shortener | Linkzar">
       <Homepage theme={theme} />

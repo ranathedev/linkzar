f8822d20235bc6b1adc06ac2b3ff09827a5ad00d
ranaintizar <intizaralirana2@gmail.com>
2024-05-13 14:09:09 +0500

    Minor Fixes

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getDatabase } from 'firebase/database'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.APIKEY,
@@ -15,6 +15,6 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
-export const db = getDatabase(app)
+export const db = getFirestore(app)
 
 export default auth
